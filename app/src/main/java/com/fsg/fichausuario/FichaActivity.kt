b8fb8745152a5f7b395907871038417433a9786d
Raul Leiva <raul.lg@hotmail.com>
2023-07-31 12:29:41 +0200

    Final

--- app/src/main/java/com/fsg/fichausuario/FichaActivity.kt
+++ app/src/main/java/com/fsg/fichausuario/FichaActivity.kt
@@ -2,12 +2,23 @@ package com.fsg.fichausuario
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class FichaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ficha)
-        var ficha = intent.getStringExtra("Login").toString()
+        var nombre = intent.getStringExtra("Login").toString()
+        if (nombre!=null){
+            var editTextNombre: EditText = findViewById(R.id.editTextNombre)
+            editTextNombre.setText(nombre)
+        }
+    }
+    fun botonGuardar(view: View){
 
+    }
+    fun botonAtras(view: View){
+        finish()
     }
 }
--- app/src/main/java/com/fsg/fichausuario/MainActivity.kt
+++ app/src/main/java/com/fsg/fichausuario/MainActivity.kt
@@ -1,32 +1,47 @@
 package com.fsg.fichausuario
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private var usuario: EditText = findViewById(R.id.editTextUsuario)
-    private var contraseña: EditText = findViewById(R.id.editTextContraseña)
-
+    lateinit var sharedPreferences:SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-
-    fun botonLogin(view: View) {
-        var ficha = Intent(this, FichaActivity::class.java)
-        ficha.putExtra("Login","Usuario")
-
-        if (usuario.equals("Usuario") && contraseña.equals("Usuario1")){
+        sharedPreferences = getSharedPreferences("Datos", Context.MODE_PRIVATE)
+        if (sharedPreferences.contains("Login")){
+            var nombreUsuario = sharedPreferences.getString("Login", "")
+            var ficha = Intent(this, FichaActivity::class.java)
+            ficha.putExtra("Login", nombreUsuario)
             startActivity(ficha)
-        }else{
+        }
+    }
+    fun botonLogin(view: View) {
+        var usuario: EditText = findViewById(R.id.editTextUsuario)
+        var contrasenia: EditText = findViewById(R.id.editTextContraseña)
+        var usuarioS = usuario.text.toString()
+        var contraseniaS = contrasenia.text.toString()
+
+        if (contraseniaS.equals((usuarioS + "1"))){
+
+            var editor = sharedPreferences.edit()
+            editor.putString("Login", usuarioS)
+            editor.commit()
+
+            var ficha = Intent(this, FichaActivity::class.java)
+            ficha.putExtra("Login", usuario.text.toString())
+            startActivity(ficha)
+        } else {
             Toast.makeText(this, "Los credenciales no son validos.", Toast.LENGTH_SHORT).show()
         }
     }
+
     fun botonSalir(view: View) {
         finish()
     }
